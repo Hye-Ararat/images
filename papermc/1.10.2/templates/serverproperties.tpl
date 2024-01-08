5a4e08e122656fbf6163d4da711a507da302140f
Wolfo-Gaming <klokko06@gmail.com>
2024-01-08 21:31:45 +0100

    Add papermc versions 1.8.8 - 1.19.4

---
+++ papermc/1.10.2/templates/serverproperties.tpl
@@ -0,0 +1,3 @@
+query.port={{ config_get("environment.PRIMARY_PORT", "25565") }}
+server-port={{ config_get("environment.PRIMARY_PORT", "25565") }}
+server-ip=0.0.0.0
---
+++ papermc/1.16.5/files/download.sh
@@ -0,0 +1,4 @@
+url='https://papermc.io/api/v2/projects/paper'
+version='1.16.5'
+build=$(curl -sSfL "$url/versions/$version"); build=${build%]*} build=${build##*[,[]}
+curl -sSfL "$url/versions/$version/builds/$build/downloads/paper-$version-$build.jar" -o paperclip.jar
